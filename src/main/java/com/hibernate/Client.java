1b8512e84502b374083b2b0da383609652b3eda5
Sandip Kumar <162599122+sandeeppal1432@users.noreply.github.com>
2024-09-16 10:25:31 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/Client.java
@@ -0,0 +1,26 @@
+package com.hibernate;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.entity.Employee;
+
+public class Client {
+
+	public static void main(String[] args) {
+      
+		Employee e= new Employee(3, "Ansh", 22, "Male" , 45784);
+		Configuration cfg = new Configuration().configure();
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(e);
+		tx.commit();
+
+		
+	}
+
+}
